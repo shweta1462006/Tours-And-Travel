749ff53a7f3fe5ed52472be04ed4b4fb4f83e928
shweta1462006 <shwetachauahan221101@gmail.com>
2025-06-26 14:51:41 +0530

    Home_update

--- Tour and travel/src/App.jsx
+++ Tour and travel/src/App.jsx
@@ -3,6 +3,7 @@ import Destinantion from './Component/Pages/Destinantion'
 import {  Route, Routes } from 'react-router-dom'
 import Nav from './Component/Pages/Nav'
 import Home from './Component/Pages/Home'
+import Layer from './Component/Pages/Layer'
 function App() {
   return(
   <>
@@ -10,6 +11,7 @@ function App() {
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/Destinantion' element={<Destinantion/>}/>
+    <Route path='/Layer' element={<Layer/>}/>
   </Routes>
 
   {/* <Destinantion/> */}

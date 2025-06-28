95941a32f753ace64d0338802b5d5b58e898d53a
shweta1462006 <shwetachauahan221101@gmail.com>
2025-06-28 15:49:27 +0530

    pudh_end

--- Tour and travel/src/Component/Pages/Home.jsx
+++ Tour and travel/src/Component/Pages/Home.jsx
@@ -1,26 +1,27 @@
 'use client'
 
-import { useState } from 'react'
 import "../CSS/Home.css";
 import Layer from './Layer'
 import Data from '../../Component/Data'
-import Section from './Section';
+import Section from './Section'
 
 export default function Home() {
   return (
     <div className="bg-white">
-      <div className="bg-[url('https://www.tourmyindia.com/blog//wp-content/uploads/2022/01/Best-Places-to-Visit-in-March-in-India.jpg')] bg-cover bg-center h-[300px] sm:h-[400px] md:h-[250px] w-full flex items-center justify-center">
+      {/* Hero Section */}
+      <div className="bg-[url('https://www.tourmyindia.com/blog//wp-content/uploads/2022/01/Best-Places-to-Visit-in-March-in-India.jpg')] bg-cover bg-center w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] flex items-center justify-center">
         <div className="text-center px-4">
-          <h1 className="text-8xl sm:text-6xl md:text-7xl lg:text-9xl font-serif font-bold text-black mb-4">
+          <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-serif font-bold text-black mb-4 leading-tight">
             India
-           </h1>
-          <p className="text-sm sm:text-base tracking-[9px] md:text-lg  text-black font-medium">
-                  We are Unveiling the distant corner of the world ...
+          </h1>
+          <p className="text-xs sm:text-sm md:text-base lg:text-lg tracking-wide text-black font-medium">
+            We are unveiling the distant corners of the world...
           </p>
         </div>
       </div>
 
-      <div className="bg-white text-gray-800 p-6 sm:p-8 md:p-12 max-w-screen-xl mx-auto">
+      {/* Main Section */}
+      <div className="bg-white text-gray-800 px-4 sm:px-6 md:px-12 lg:px-20 py-8 md:py-12 max-w-screen-xl mx-auto">
         <Section />
         <Data />
         <Layer />
--- Tour and travel/src/Component/Pages/TourPlaces.jsx
+++ Tour and travel/src/Component/Pages/TourPlaces.jsx
@@ -1,67 +1,71 @@
-import React from 'react'
+import React from 'react';
 
 export default function() {
   return (
     <>
-    <div className='flex  flex-row justify-evenly w-full h-96 mt-8'>
-<div className='rounded-lg  brightness-75 w-96 '>
-<img src="/public/Images/climbing.webp" alt="Climbimg"  className='h-full w-full'/>
-<div className="absolute inset-0 ">
-        <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-4 rounded">
-          Trakking
-        </h2>
-        <button class="rounded-xl h-10 w-40 shadow-lg mt-64 mx-24 shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 align-center">Learn More</button>
-      </div> 
+      <div className="flex flex-col md:flex-row justify-evenly w-full mt-8 space-y-6 md:space-y-0 md:h-96">
+        <div className="relative rounded-lg brightness-75 w-full md:w-96 h-64 md:h-full">
+          <img src="/public/Images/climbing.webp" alt="Climbing" className="h-full w-full object-cover rounded-lg" />
+          <div className="absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-30 rounded-lg">
+            <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-2 rounded">Trakking</h2>
+            <button className="rounded-xl h-10 w-40 shadow-lg shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 self-center">
+              Learn More
+            </button>
+          </div>
+        </div>
 
-</div>
-<div className='rounded-lg brightness-75  w-96'>
-<img src="/public/Images/ride.webp" alt="bike ride" className='h-full w-full' />
-<div className="absolute inset-0 ">
-        <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-4 rounded">
-          Bike Ride
-        </h2>
-        <button class="rounded-xl h-10 w-40 shadow-lg mt-64 mx-24 shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 align-center">Learn More</button>
+        <div className="relative rounded-lg brightness-75 w-full md:w-96 h-64 md:h-full">
+          <img src="/public/Images/ride.webp" alt="Bike Ride" className="h-full w-full object-cover rounded-lg" />
+          <div className="absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-30 rounded-lg">
+            <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-2 rounded">Bike Ride</h2>
+            <button className="rounded-xl h-10 w-40 shadow-lg shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 self-center">
+              Learn More
+            </button>
+          </div>
+        </div>
+
+        <div className="relative rounded-lg brightness-75 w-full md:w-96 h-64 md:h-full">
+          <img src="/public/Images/safari.webp" alt="Safari" className="h-full w-full object-cover rounded-lg" />
+          <div className="absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-30 rounded-lg">
+            <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-2 rounded">Jeep Safari</h2>
+            <button className="rounded-xl h-10 w-40 shadow-lg shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 self-center">
+              Learn More
+            </button>
+          </div>
+        </div>
       </div>
-</div>
-<div className='rounded-lg brightness-75  w-96'>
-  <img src="/public/Images/safari.webp" alt="safari" className='h-full w-full' />
-  <div className="absolute inset-0 ">
-        <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-4 rounded">
-          Jeep Safari
-        </h2>
-        <button class="rounded-xl h-10 w-40 shadow-lg mt-64 mx-24 shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 align-center">Learn More</button>
+
+      <div className="flex flex-col md:flex-row justify-evenly w-full mt-8 space-y-6 md:space-y-0 md:h-96">
+        <div className="relative rounded-lg brightness-75 w-full md:w-96 h-64 md:h-full">
+          <img src="/public/Images/spritiual.webp" alt="Spiritual" className="h-full w-full object-cover rounded-lg" />
+          <div className="absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-30 rounded-lg">
+            <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-2 rounded">Spiritual Places</h2>
+            <button className="rounded-xl h-10 w-40 shadow-lg shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 self-center">
+              Learn More
+            </button>
+          </div>
+        </div>
+
+        <div className="relative rounded-lg brightness-75 w-full md:w-96 h-64 md:h-full">
+          <img src="/public/Images/valley.webp" alt="Valley" className="h-full w-full object-cover rounded-lg" />
+          <div className="absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-30 rounded-lg">
+            <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-2 rounded">Local Valley Of Ladakh</h2>
+            <button className="rounded-xl h-10 w-40 shadow-lg shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 self-center">
+              Learn More
+            </button>
+          </div>
+        </div>
+
+        <div className="relative rounded-lg brightness-75 w-full md:w-96 h-64 md:h-full">
+          <img src="/public/Images/Frozen river trak.webp" alt="Frozen River" className="h-full w-full object-cover rounded-lg" />
+          <div className="absolute inset-0 flex flex-col justify-between p-4 bg-black bg-opacity-30 rounded-lg">
+            <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-2 rounded">Frozen River</h2>
+            <button className="rounded-xl h-10 w-40 shadow-lg shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 self-center">
+              Learn More
+            </button>
+          </div>
+        </div>
       </div>
-</div>
-    </div>
-     <div className='flex flex-row justify-evenly w-full h-96 mt-8'>
-<div className='rounded-lg brightness-75 w-96 '>
-<img src="/public/Images/spritiual.webp" alt="Spritiual"  className='h-full w-full'/>
-<div className="absolute inset-0 ">
-        <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-4 rounded">
-         Spritiual Places
-        </h2>
-        <button class="rounded-xl h-10 w-40 shadow-lg mt-64 mx-24 shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 align-center">Learn More</button>
-      </div>
-</div>
-<div className='rounded-lg brightness-75 w-96'>
-  <img src="/public/Images/valley.webp" alt="Valley" className='h-full w-full' />
-  <div className="absolute inset-0 ">
-        <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-4 rounded">
-          Local Valley Of Ladakh
-        </h2>
-        <button class="rounded-xl h-10 w-40 shadow-lg mt-64 mx-24 shadow-orange-900 hover:bg-orange-600 hover:text-white bg-orange-500 align-center">Learn More</button>
-      </div>
-</div>
-<div className='rounded-lg brightness-75 w-96'>
-  <img src="/public/Images/Frozen river trak.webp" alt="Frozen"  className='h-full w-full'/>
-  <div className="absolute inset-0 ">
-        <h2 className="text-white text-xl font-bold bg-black bg-opacity-50 p-4 rounded">
-          Frozen River
-        </h2>
-        <button class="rounded-xl h-10 w-40 shadow-lg mt-64 mx-24 shadow-orange-900 hover:bg-blue hover:bg-orange-600 hover:text-white bg-orange-500 align-center">Learn More</button>
-      </div>
-</div>
-    </div>
     </>
-  )
+  );
 }
